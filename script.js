fe9da261429dc98b432912108399dbd0b691927f
New User <newuser@News-Air.fios-router.home>
2020-10-02 22:29:03 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+// Write your code below:
+
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+  };
+  
+  const compareGuesses = (userGuess,computerGuess,secretNumber) => {
+  
+  
+    if(userGuess === computerGuess) {
+      return true
+    } else if (secretNumber <= userGuess) {
+      if(userGuess < computerGuess) {
+        return true
+      } else {
+        return false
+      }
+    } else if (secretNumber >= userGuess) {
+        if (userGuess > computerGuess) {
+          return true
+        } else {
+          return false
+        }
+    }
+  };
+  
+  const updateScore = winner => {
+    if (winner === 'human') {
+      humanScore++
+    } else if (winner === 'computer') {
+      computerScore++
+    }
+  };
+  
+  const advanceRound = () => {
+    currentRoundNumber++
+    
+  };
+  
